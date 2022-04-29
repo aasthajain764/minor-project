84afd7d583a84cd90ced1ef06d258bac2e918517
aasthajain764 <aasthsjain764@gmail.com>
2022-04-29 17:33:12 +0530

    Initial commit

---
+++ msg.php
@@ -0,0 +1,6 @@
+<!DOCTYPE html>
+<head><title>Message</title></head>
+<body style="background-color:#013d47; color:#fff; text-align:center; vertical-align:center;">
+<br><br><br><br><br><br><br><br><br><br><br><br><br>
+<h1>Your message has been sent!</h1>
+</body>
